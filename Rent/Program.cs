d7f1167ef2219f8f1dee19f599e5db602755ae19
RaimondasTvaska <rtvaska@gmail.com>
2021-06-26 08:41:34 +0300

    Add project files.

---
+++ Rent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Rent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Rentable house = new Rentable();
+            Console.WriteLine(house.Available);
+
+            /* Rentable
+             * Available (is,has,should signalizuoja, kad tai bus boolean funkcijose) true false
+             * beds int
+             * kitchen true false
+             * shower true false
+             * floor int
+             * name
+             * price
+             * */
+        }
+    }
+}
---
+++ Rent/Rentable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rent
+{
+    class Rentable
+    {
+        //sioje klaseje apibreziama, kaip turi atrodyti vienas dalykas (objektas) Sablonas
+
+
+        public bool Available { get; set; }
+        public int Beds { get; set; }
+        public bool Kitchen { get; set; }
+        public bool Shower { get; set; }
+        public int Floor { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+
+    }
+}
